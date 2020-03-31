6088af877c11f56ef4f5f626a6684a92fd0849bd
shainaztamboli <shainaztamboli@gmail.com>
2020-03-31 18:56:11 +0530

    Spring Boot JWT Sample project

---
+++ src/main/java/com/sample/demo/SpringJwtApplication.java
@@ -0,0 +1,51 @@
+package com.sample.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sample.demo.model.Role;
+import com.sample.demo.model.User;
+import com.sample.demo.service.UserService;
+
+@SpringBootApplication
+public class SpringJwtApplication implements CommandLineRunner {
+
+	@Autowired
+	UserService userService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... params) throws Exception {
+		User admin = new User();
+		admin.setUsername("admin");
+		admin.setPassword("admin");
+		admin.setEmail("admin@email.com");
+		admin.setRoles(new ArrayList<Role>(Arrays.asList(Role.ROLE_ADMIN)));
+
+		userService.signup(admin);
+
+		User client = new User();
+		client.setUsername("client");
+		client.setPassword("client");
+		client.setEmail("client@email.com");
+		client.setRoles(new ArrayList<Role>(Arrays.asList(Role.ROLE_CLIENT)));
+
+		userService.signup(client);
+	}
+
+}
